297567e8c76c263c0156886971baef8bc169ddd9
Johan Vergeer <johan@redgyro.com>
2017-11-12 16:18:05 +0100

    Run compilemessages from manage.py

--- compose/production/django/gunicorn.sh
+++ compose/production/django/gunicorn.sh
@@ -6,5 +6,5 @@ set -o nounset
 
 
 python /app/manage.py collectstatic --noinput
-python django-admin compilemessages
+python /app/manage.py compilemessages
 /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
